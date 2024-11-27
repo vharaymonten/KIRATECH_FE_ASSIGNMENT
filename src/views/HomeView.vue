e6396d32a9b857f4e5a1e02707fb92dcac253ef4
jivharaymond.lie <vharaymonten@gmail.com>
2024-11-27 20:01:28 +0700

    fix styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -90,14 +90,14 @@ const changeSortOrder = () => {
   <div>
     <div class="flex items-center space-x-2 mb-4">
       <!-- Search Input -->
-      <div class="relative w-full">
+      <div class="relative w-8/12">
         <input
           type="text"
           placeholder="Search..."
           v-model="sortSearch.name"
           class="w-full px-4 py-2 border rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:outline-none focus:border-blue-500"
         />
-        <div class="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-400">
+        <div class="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-400 w-4/12">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
